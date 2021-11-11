a07b9a3fd8373b2fcba21646c2669dc9e79ba19f
DanbiLim <win5450@gmail.com>
2021-11-11 15:47:00 +0900

    first kakao commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Friends from './routes/Friends';
+import Profile from './routes/Profile';
+import Chats from './routes/Chats';
+import Chatting from './routes/Chatting';
+import Find from './routes/Find';
+import More from './routes/More';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Friends />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/chatting" element={<Chatting />} />
+        <Route path="/find" element={<Find />} />
+        <Route path="/more" element={<More />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
